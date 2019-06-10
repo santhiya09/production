c6c618d27b01773c30d9edcb3b8bc34bf67b4afd
Santhiya Palaniyappan <santhiya.palaniyappan@rrd.com>
2019-06-10 18:51:43 +0530

    Updates

---
+++ precache-manifest.7aad21187091385556f1ef15fc43d5c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867664995b28ce6d2a0dca0ae428238f",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "04c83a49b7aeb2b179fc",
+    "url": "/production/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/production/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "04c83a49b7aeb2b179fc",
+    "url": "/production/static/js/main.b923e686.chunk.js"
+  },
+  {
+    "revision": "6af66b1dc346966c06ee",
+    "url": "/production/static/js/runtime~main.2910c19d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/production/static/media/logo.5d5d9eef.svg"
+  }
+]);
